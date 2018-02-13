acfa68e66e33ae121df1fa62eac540c1d7992865
Oleksandr Nykytyuk <34703970+OleksandrNyk@users.noreply.github.com>
2018-02-13 23:00:27 +0200

    Update task1.js

--- homework_07/js/task1.js
+++ homework_07/js/task1.js
@@ -7,37 +7,11 @@ if ( num < 1 || num > 20 || isNaN(parseInt(num))) {
 } else {
 	
 	 var line = "";
-  for (var i = 1; i <= num; i++) {
-    for (var j = 1; j <= i; j++) {
-      line += unit;
-    }
-    line += "\n";
-  }
-  console.log(line);		
+         for (var i = 1; i <= num; i++) {
+         for (var j = 1; j <= i; j++) {
+         line += unit;
+         }
+         line += "\n";
+         }
+         console.log(line);		
 }
-
-
-
-
-/*
-var num = parseInt(prompt("Please enter number between 1 and 20", " "));
-var unit = "[~]";
-
-if ( num < 1 || num > 20 || isNaN(parseInt(num))) {
-	console.error('Incorrect!');
-} else {
-	
-	for(var i = 0; i < num; i++) 
-{
-	var block = '';
-	for(var x = 1; x < num-i; x++){
-		block = block + '   ';
-	}
-	for(var m = 1; m <= (2*i+1); m++){
-		block = block + unit;
-	}
-	console.log(block);
-}
-			
-}
-*/
