ddd1ad597ecd7fbd2b2952a92aee9666fdc85316
Oleksandr Nykytyuk <34703970+OleksandrNyk@users.noreply.github.com>
2018-02-13 23:05:20 +0200

    Update task1.js

--- homework_07/js/task1.js
+++ homework_07/js/task1.js
@@ -5,13 +5,12 @@ var unit = "[~]";
 if ( num < 1 || num > 20 || isNaN(parseInt(num))) {
 	console.error('Incorrect!');
 } else {
-	
-	 var tier = "";
-         for (var i = 1; i <= num; i++) {
-         for (var j = 1; j <= i; j++) {
-         tier += unit;
-         }
-         tier += "\n";
-         }
-         console.log(tier);		
+		var tier = "";
+     	for (var i = 1; i <= num; i++) {
+        	for (var j = 1; j <= i; j++) {
+        	tier += unit;
+         	}
+        tier += "\n";
+        }
+   console.log(tier);		
 }
